792326ba4c41c7092d9770246bfbe3800c55412e
wangyanfei <wangyf6@sugon.com>
2023-05-26 08:34:28 +0800

    [feat] update deploy

--- deploy.sh
+++ deploy.sh
@@ -37,13 +37,18 @@ kubectl -n webhook-demo create secret tls webhook-server-tls \
     --cert "${keydir}/webhook-server-tls.crt" \
     --key "${keydir}/webhook-server-tls.key"
 
+
 # Read the PEM-encoded CA certificate, base64 encode it, and replace the `${CA_PEM_B64}` placeholder in the YAML
 # template with it. Then, create the Kubernetes resources.
 ca_pem_b64="$(openssl base64 -A <"${keydir}/ca.crt")"
-sed -e 's@${CA_PEM_B64}@'"$ca_pem_b64"'@g' <"${basedir}/deployment.yaml.template" \
-    | kubectl create -f -
+echo "[ca_pem_b64]" $ca_pem_b64
+cp ${basedir}/deployment.yaml.template ${basedir}/deployment.yaml
+#sed -e 's@${CA_PEM_B64}@'"$ca_pem_b64"'@g' ${basedir}/deployment.yaml
+#kubectl apply -f ${basedir}/deployment.yaml
+sed -e 's@${CA_PEM_B64}@'"$ca_pem_b64"'@g' <"${basedir}/deployment.yaml" \
+    | kubectl apply -f -
 
 # Delete the key directory to prevent abuse (DO NOT USE THESE KEYS ANYWHERE ELSE).
-rm -rf "$keydir"
+#rm -rf "$keydir"
 
 echo "The webhook server has been deployed and configured!"
